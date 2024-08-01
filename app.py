c70a6155fc83028c9c0ca267f5c67cf6281d16fd
S Khadar Basha <khadarbasha@hgtlkkd.com>
2024-08-01 22:39:04 +0530

    replace header image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title('CampusX')
 col1,col2=st.columns(2)
 
 with col1:
-    st.image('datascience_1718760959885.jpg')
+    st.image('DF4B0293.png')
 
 with col2:
     st.header('CampusX is an online platform')
