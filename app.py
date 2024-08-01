cd675455343796045232434507150145493ffbce
S Khadar Basha <khadarbasha@hgtlkkd.com>
2024-08-01 09:22:09 +0530

    add image to the home page

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('CampusX')
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.image('datascience_1718760959885.jpg')
+
+with col2:
+    st.header('CampusX is an online platform')
